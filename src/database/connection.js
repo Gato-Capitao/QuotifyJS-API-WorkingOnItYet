19e7785371aaeaee0d378f7a1a3b5fa891620a76
Adson Gabriel <99619952+Gato-Capitao@users.noreply.github.com>
2024-02-22 19:52:39 +0000

    Create connection.js

---
+++ src/database/connection.js
@@ -0,0 +1,16 @@
+import { Sequelize } from "sequelize"
+
+export const database = new Sequelize("db_quotify", "root", "",{
+    host: "localhost",
+    dialect: "mysql"
+})
+
+export async function tryToConnect(){
+    try{
+        database.sync({force: true})
+        await database.authenticate()
+        console.log("Connection Established")
+    }catch (error){
+        console.log("Error trying to establish connection")
+    }
+}
