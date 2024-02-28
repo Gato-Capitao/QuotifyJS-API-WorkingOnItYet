1b7f56bef298a6a80c48f403a053939921d19c7a
Adson Gabriel <99619952+Gato-Capitao@users.noreply.github.com>
2024-02-28 18:23:03 +0000

    Criando rotas para as funções CRUD

--- src/routes/User.routes.js
+++ src/routes/User.routes.js
@@ -1,10 +1,27 @@
 import { Router } from "express"
-import { createUser } from "../controllers/User.controller.js"
+import { createUser, getUser, updateUsername, updateUsersPassword, deleteUser } from "../controllers/User.controller.js"
 import { IsBodyValid } from "../middleware/userValidation.js"
 
-export const routerUser = Router()
+const routerUser = Router()
 
 routerUser.post("/register-user", IsBodyValid, async (req, res) =>{
     return await createUser(req, res)
 })
 
+routerUser.post("/user/get", async (req, res) => {
+    return await getUser(req, res)
+})
+
+routerUser.post("/user/update-username", async (req, res) => {
+    return await updateUsername(req, res)
+})
+
+routerUser.post("/user/update-password", async (req, res) => {
+    return await updateUsersPassword(req, res)
+})
+
+routerUser.post("/user/delete-account", async (req, res) => {
+    return await updateUsersPassword(req, res)
+})
+
+export {routerUser}
