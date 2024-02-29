cf692ea68df7f883adc9ee9f9a15e3917a49d296
Adson Gabriel <99619952+Gato-Capitao@users.noreply.github.com>
2024-02-29 14:07:37 +0000

    Adição de mensagens prontas

---
+++ src/shared/messages.js
@@ -0,0 +1,10 @@
+export const ERROS = {
+    WRONG_PASSWORD: "Wrong password!",
+    NOT_FOUND: "not found!"
+}
+
+export const SUCCESS = {
+    USER: "user successfully!",
+    UPDATED: "updated successfully!",
+    DELETED: "deleted successfully!",
+}
