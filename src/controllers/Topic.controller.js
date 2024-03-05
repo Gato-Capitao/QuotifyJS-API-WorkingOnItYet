12be87e33fe489d6b3280af923a776ad88ab6d32
Adson Gabriel <99619952+Gato-Capitao@users.noreply.github.com>
2024-03-05 14:13:08 +0000

    Update Topic.controller.js

--- src/controllers/Topic.controller.js
+++ src/controllers/Topic.controller.js
@@ -1,12 +1,13 @@
 import { TopicModel } from "../models/Topic.model.js"
-import { database } from "../database/connection.js"
+import { TopicService } from "../services/Topic.service.js"
+
+const instanceTopicService = new TopicService()
 
 export async function createTopic(req, res){
     try{
-        const {title, description} = req.body
-        await database.sync()
-        await TopicModel.create({title, description})
-        return res.status(201).json({message:"Created topic sucessfully!"})
+        const {title, description, userId} = req.body
+        const response = await instanceTopicService.createTopic(title, description, userId)
+        return res.status(response.statusValue).json({message: response.message})
     }catch(error){
         return res.status(404).json({error: error.message})
     }
