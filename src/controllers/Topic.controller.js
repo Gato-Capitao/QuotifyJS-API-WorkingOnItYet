e659a5bb71fe57ba0f27ea552cf2f4251016c35b
Adson Gabriel <99619952+Gato-Capitao@users.noreply.github.com>
2024-03-07 14:02:31 +0000

    Adicionando novas rotas

--- src/controllers/Topic.controller.js
+++ src/controllers/Topic.controller.js
@@ -28,3 +28,23 @@ export async function updateTitle(req, res){
     }
 }
 
+export async function updateDescription(req, res){
+    try{
+        const { newDescription, topicId, userId, password } = req.body
+        const response = await instanceTopicService.updateDescription(userId, topicId, password, newDescription)
+        return res.status(response.statusValue).json({response})
+    }catch(error){
+        return res.status(404).json({error: error.message})
+    }
+}
+
+export async function deleteTopic(req, res){
+    try{
+        const { userId, topicId, password} = req.body
+        const response = await instanceTopicService.deleteTopic(userId, topicId, password)
+        
+        return res.status(response.statusValue).json({response})
+    }catch(error){
+        return res.status(404).json({error: error.message})
+    }
+}
