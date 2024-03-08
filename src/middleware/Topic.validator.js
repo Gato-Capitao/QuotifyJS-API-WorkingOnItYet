ebdc3fadbfd09de3d2fed1897e8af867accae5ff
Adson Gabriel <99619952+Gato-Capitao@users.noreply.github.com>
2024-03-08 13:30:36 +0000

    Update Topic.validator.js

--- src/middleware/Topic.validator.js
+++ src/middleware/Topic.validator.js
@@ -1,8 +1,9 @@
 import { ERROS } from "../shared/messages";
 export class TopicValidator{
-    async createTopicValidator(){
+    async createTopicValidation(req, res, next){
         const { title, description, creatorId } = req.body || {}
         const fields = ["title", "description", "creatorId"]
+        const errors = []
 
         for (const field of fields) {
             if (!req.body[field]) {
@@ -17,7 +18,61 @@ export class TopicValidator{
         next()
     }
 
-    async getTopicValidator(){
+    async getTopicValidation(req, res, next){
+        const { id } = req.body || {}
+        const fields = ["id"]
+        const erros = []
+
+        for(const field of fields){
+            if(!req.body[field]){
+                erros.push(`${ERROS.TOPIC_NEEDS} a/an ${field}`)
+            }
+        }
+
+        if(erros.length){
+            return res.status(404).json({message: erros})
+        }
+
+        next()
+    }
+
+    async updateTitleValidation(req, res, next){
+        const { newTitle, topicId, userId, password } = req.body || {}
+        const fields = ["newTitle", "topicId", "userId", "password"]
+        const erros = []
+
+        for(const field of fields){
+            if(!req.body[field]){
+                erros.push(`${ERROS.TOPIC_NEEDS} a/an ${field}`)
+            }
+        }
+
+        if(erros.length){
+            return res.status(404).json({message: erros})
+        }
+
+        next()
+    }
+
+    async updateDescriptionValidation(req, res, next){
+        const { newDescription, topicId, userId, password } = req.body || {}
+        const fields = ["newDescription", "topicId", "userId", "password"]
+        const erros = []
+
+        for(const field of fields){
+            if(!req.body[field]){
+                erros.push(`${ERROS.TOPIC_NEEDS} a/an ${field}`)
+            }
+        }
+
+        if(erros.length){
+            return res.status(404).json({message: erros})
+        }
+
+        next()
+    }
+
+    async deleteTopicValidation(req, res, next){
         const { id } = req.body || {}
         const fields = ["id"]
         const erros = []
