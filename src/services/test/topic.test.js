9a8cd2c5a2f10b371771ce63057999e0c55a83f6
Adson Gabriel <99619952+Gato-Capitao@users.noreply.github.com>
2024-03-11 18:06:32 +0000

    Update topic.test.js

--- src/services/test/topic.test.js
+++ src/services/test/topic.test.js
@@ -1,10 +1,22 @@
 import { TopicModel } from "../../models/Topic.model.js"
-import { TopicService } from "../User.service.js"
+import { TopicService } from "../Topic.service.js"
+import { UserService } from "../User.service.js"
+import { UserModel } from "../../models/User.model.js"
 
 const instanceTopicService = new TopicService()
+const instanceUserService = new UserService()
+
+afterEach(async () => {
+    await TopicModel.destroy({where: {}})
+    await UserModel.destroy({where: {}})
+});
 
 test("Can create a topic", async ()=>{
-    const response = await instanceTopicService.createTopic("Test", "Apenas um mero teste", "")
+    const user1 = await instanceUserService.createUser("test", "user1@test.test", "test_test")
+    const response = await instanceTopicService.createTopic("Resilience", "Quotes about resilience", user1.id)
 
+    console.log(response)
     expect(response.statusValue).toEqual(201)
 })
+
+
