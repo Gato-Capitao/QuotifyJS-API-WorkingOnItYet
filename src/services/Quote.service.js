12bb4d8d15d953b0b68804eb750f8e9f8b92a3c0
Adson Gabriel <99619952+Gato-Capitao@users.noreply.github.com>
2024-03-13 14:29:03 +0000

    Update Quote.service.js
    Creating delete quote function

--- src/services/Quote.service.js
+++ src/services/Quote.service.js
@@ -1,6 +1,8 @@
 import { database } from "../database/connection.js"
 import { QuoteModel } from "../models/Quote.model.js"
-import { SUCCESS } from "../shared/messages.js"
+import { UserModel } from "../models/User.model.js"
+import { TopicModel } from "../models/Topic.model.js"
+import { SUCCESS, ERROS } from "../shared/messages.js"
 
 export class QuoteService{
     async createQuote(quote, creatorId, topicId){
@@ -16,6 +18,30 @@ export class QuoteService{
             return {
                 message: error.message,
                 statusValue: error.statusValue
+            }
+        }
+    }
+
+    async deleteQuote(quoteId, userId, password){
+        try{
+            await database.sync()
+            const quote = await QuoteModel.findByPk(quoteId)
+            const topic = await TopicModel.findByPk(quote.topicId)
+            const user = await UserModel.findByPk(userId)
+
+            if(quote.creatorId!==user.id || topic.creatorId !== user.id){return {statusValue: 404, message: "Only the creator of the quote or topic can delete it!"}}
+            else if(user.password !== password){return {statusValue: 404, message: ERROS.WRONG_PASSWORD}}
+
+            await quote.destroy()
+
+            return {
+                statusValue: 200,
+                message: `Deleted ${ERROS.QUOTE}`
+            }
+        }catch(error){
+            return {
+                statusValue: error.statusValue,
+                message: error.message
             }
         }
     }
