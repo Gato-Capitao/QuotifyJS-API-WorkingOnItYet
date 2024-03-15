3be07ffb53c8241b088e683aeaa8d86c50c9a05b
Adson Gabriel <99619952+Gato-Capitao@users.noreply.github.com>
2024-03-15 15:06:14 +0000

    Update user.test.js

--- src/services/test/user.test.js
+++ src/services/test/user.test.js
@@ -8,27 +8,37 @@ afterEach(async ()=>{
 const instanceUserService = new UserService()
 
 test("Can create a user", async ()=>{
-  const reponseUser = await instanceUserService.createUser("test", "test@user.test", "test_test")
+    const reponseUser = await instanceUserService.createUser("test", "test@user.test", "test_test")
 
-  expect(reponseUser.statusValue).toEqual(201)
+    expect(reponseUser.statusValue).toEqual(201)
 })
 
 test("Can get user by id", async ()=> {
-  const responseUser = await instanceUserService.createUser("test", "test@user.test", "test_test")
+    const responseUser = await instanceUserService.createUser("test", "test@user.test", "test_test")
 
-  const result = await instanceUserService.getUser(responseUser.userId)
+    const result = await instanceUserService.getUser(responseUser.userId)
 
-  expect(result.statusValue).toEqual(200)
+    expect(result.statusValue).toEqual(200)
 })
 
-test("The user can update password and only using the right password", async ()=>{
-  const responseUser = await instanceUserService.createUser("test", "test@user.test", "RIGHT_password")
+test("The user can update password only using the right password", async ()=>{
+    const responseUser = await instanceUserService.createUser("test", "test@user.test", "RIGHT_password")
 
-  const resultWrongPassword = await instanceUserService.updatePassword(responseUser.userId, "WRONG_password", "UShouldntBeAbleToUpdateIt")
+    const resultWrongPassword = await instanceUserService.updatePassword(responseUser.userId, "WRONG_password", "UShouldntBeAbleToUpdateIt")
 
-  const resultRightPassword = await instanceUserService.updatePassword(responseUser.userId, "RIGHT_password", "UMustBeAbleToUpdateIt")
+    const resultRightPassword = await instanceUserService.updatePassword(responseUser.userId, "RIGHT_password", "UMustBeAbleToUpdateIt")
 
-  expect(resultWrongPassword.statusValue).toEqual(404)
-  expect(resultRightPassword.statusValue).toEqual(200)
+    expect(resultWrongPassword.statusValue).toEqual(404)
+    expect(resultRightPassword.statusValue).toEqual(200)
 })
 
+test("The user can update username only using the right password", async ()=>{
+    const responseUser = await instanceUserService.createUser("test", "test@user.test", "RIGHT_password")
+
+    const resultWrongPassword = await instanceUserService.updateUsername(responseUser.userId, "WRONG_password", "newUsername")
+
+    const resultRightPassword = await instanceUserService.updateUsername(responseUser.userId, "RIGHT_password", "newUsername")
+
+    expect(resultWrongPassword.statusValue).toEqual(404)
+    expect(resultRightPassword.statusValue).toEqual(200)
+})
