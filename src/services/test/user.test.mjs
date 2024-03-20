741cb0216b4adcc12791aa7acf2c33d3b9e7cf15
Adson Gabriel <adsongabrielfsilva@gmail.com>
2024-03-20 13:55:30 +0000

    Update user.test.mjs

--- src/services/test/user.test.mjs
+++ src/services/test/user.test.mjs
@@ -20,4 +20,16 @@ describe("UserService", ()=>{
             password: "test_test"
         })
     })
+
+    test("Can delete a user", async ()=>{
+        const service = new UserService()
+
+        const responseCreatUser = await service.createUser("test", "test@user.test", "test_test")
+
+        expect(responseCreatUser.statusValue).toEqual(201)
+
+        const responseDeleteUset = await service.deleteUser(responseCreatUser.userId)
+
+        expect(responseCreatUser.statusValue).toEqual(200)
+    })
 })
